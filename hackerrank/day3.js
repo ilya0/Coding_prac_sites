ac9f4752fbfb70b1b0eb18c591e5925d1144df4a
ilya0 <iosovets@gmail.com>
2016-11-09 14:39:33 -0800

    working code

--- hackerrank/day3.js
+++ hackerrank/day3.js
@@ -1,14 +1,21 @@
 var rotatearray = function(arrayinput,rotation){
+
   var temparray = arrayinput;
   var rotatecounter = 0;
 
   while(rotatecounter < rotation){
     var rotateholder = temparray.slice(0,1);
     temparray.shift();
-    temparray.push(roatateholder)
-
-
-    roatatecounter++;
+    temparray.push(rotateholder)
+    rotatecounter++;
 
   }
+  console.log(temparray);
 }
+var testarray = ["1","2","3","a","b","c"]
+
+
+rotatearray(testarray,3);
+
+
+//code works
