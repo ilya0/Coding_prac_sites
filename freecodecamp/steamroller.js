826c7310a326224e1a49145e94b9d4e65db95304
ilya0 <iosovets@gmail.com>
2018-04-19 16:43:14 -0700

    steamroller

--- freecodecamp/steamroller.js
+++ freecodecamp/steamroller.js
@@ -1,7 +1,23 @@
-function steamrollArray(arr) {
-    // I'm a steamroller, baby
-    return arr;
-  }
+//recursive flattening
+
+var data = [[1,2,3], [7], [8,9,0]];
+
+var flatten = function(arr){
+  var flatMap = []; // int Array
   
-  steamrollArray([1, [2], [3, [[4]]]]);
+  arr.forEach(function(value){ // foreach loop on every element
   
+    if(Array.isArray(value)){ // if element is an array
+      flatMap = flatMap.concat(flatten(value)) // concatenate and re run recursion
+    }
+    else{ // if the element is not push it into flat map array
+      flatMap.push(value)
+    }
+    
+  });
+  return flatMap;
+}
+
+var result = flatten(data);
+
+console.log(result);
