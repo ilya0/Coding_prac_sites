ad077696ff7fd00bac887f4bf6cd0387f194d2ba
ilya0 <iosovets@gmail.com>
2018-04-20 11:21:36 -0700

    fib

--- freecodecamp/fibonacci.js
+++ freecodecamp/fibonacci.js
@@ -1,6 +1,19 @@
 
+
 function sumFibs(num) {
-    return num;
-  }
-  
-  sumFibs(4);
+
+  var n1 = 1;
+  var n2 = 1;
+  var n3;
+  console.log(n1);
+  console.log(n2);
+  for(var i = 0; i<num; i++){
+    n3 = n1 + n2;
+    
+    console.log(n3);
+    n1 = n2;
+    n2 = n3;
+}
+}
+
+sumFibs(1);
