1b614458f8b38af44951fafe0ac5170aafc1eeae
ilya0 <iosovets@gmail.com>
2018-04-21 15:00:56 -0700

    arguments

---
+++ freecodecamp/argumentsoptional.js
@@ -0,0 +1,18 @@
+  function addTogether() {
+      console.log(arguments[0]);
+      console.log(arguments[1]);
+      if (isNaN(arguments[0]) == true || isNaN(arguments[1] == true)) {
+          return "undefined";
+      } else {
+          return arguments[0] + arguments[1];
+
+      }
+
+
+      // if theres is two arguments
+      // do this
+      //else 
+      //run
+      return false;
+  }
+  addTogether("http://bit.ly/IqT6zt")
