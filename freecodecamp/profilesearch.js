7619ccf2b3ece34ed006351f36775ca50e6838ea
ilya0 <iosovets@gmail.com>
2018-05-01 23:08:10 -0700

    working

--- freecodecamp/profilesearch.js
+++ freecodecamp/profilesearch.js
@@ -1,50 +1,63 @@
-//Setup
-var contacts = [{
-    "firstName": "Akira",
-    "lastName": "Laine",
-    "number": "0543236543",
-    "likes": ["Pizza", "Coding", "Brownie Points"]
-},
-{
-    "firstName": "Harry",
-    "lastName": "Potter",
-    "number": "0994372684",
-    "likes": ["Hogwarts", "Magic", "Hagrid"]
-},
-{
-    "firstName": "Sherlock",
-    "lastName": "Holmes",
-    "number": "0487345643",
-    "likes": ["Intriguing Cases", "Violin"]
-},
-{
-    "firstName": "Kristian",
-    "lastName": "Vos",
-    "number": "unknown",
-    "likes": ["Javascript", "Gaming", "Foxes"]
-}
-];
 
-
-
-function lookUpProfile(firstName, prop) {
-
-    for (i = 0; i < contacts.length; i++){ // go into each sub array and check the name
-        
-        if (contacts[i].firstName === firstName) { // if name exists change name to firstN
-            if (contacts[i].hasOwnProperty(prop)){
-                return contacts[i][prop];
-            } else {
-                return "No such property";
-            }
-        }
-       
-      
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
     }
- return "No such contact";
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+   //if artists
+  if(prop == "artist"){
+    // if the property is artists then set value to artist
+    if(prop == ""){
+      //do nothing
+    }else{
+       collection[id].artist = value;
+    }
+   
+    
+  }else if(prop == "tracks"){
+    //if prop is tracks check for undefined, set new or push a new track
+    
+    if(collection[id].tracks == "undefined"){ 
+        collection[id].push({'tracks': []});
+      }else{
+        //nothing
+      }
+    //push a new track
+    collection[id].tracks.push(value);
+  }else{
+    return "nothing done";
+  }
+  // if tracks
+  
+  
+  return collection;
 }
 
-
-
-// Change these values to test your function
-lookUpProfile("Kristian", "lastName");
+// Alter values below to test your code
+updateRecords(1245, "tracks", "Addicted to Love");
