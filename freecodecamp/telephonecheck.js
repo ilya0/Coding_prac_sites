abcdf957568c2233671f4296a5eb9d3f687f530d
ilya0 <iosovets@gmail.com>
2018-05-21 17:39:22 -0700

    paren

--- freecodecamp/telephonecheck.js
+++ freecodecamp/telephonecheck.js
@@ -1,27 +1,48 @@
 
 function telephoneCheck(str) {
-    //take out -
-    // taje out ( and  )
-    // take out spaces
-    
-    //tests
-    //first number is 1
-    //has 7 digits
-    var string = str.replace(/-|\s/g,"");
-    
-      if(string.length > 11){
-        return false;
-      }else if(string[0] != 1){
-        return false;
-      }
-        else if(string.match(/[a-z]/gi) == []){
-          return true;
-        }
+  //take out -
+  // taje out ( and  )
+  // take out spaces
+  
+  //tests
+  //first number is 1
+  //has 7 digits
+  var string = str.replace(/(\(|\))|[a-z]|-|\s/gi,"");
+  console.log(string);
+    if(string.length > 11 || string.length < 10 ){
+       console.log("greater than 11");
+      return false;
+     
+    }else if(string[0] != 1 && string.length == 11){
+      console.log("1 is not first");
+      return false;
       
-      
-      }
+    }
+      else if(string.match(/[a-z]/gi) == []){
+       
+       console.log("letter found");
+        return true;
+        
+      }else if(isNaN(string[0]) == true){
+               return false;
+       }else if(string){
+         //need case for matching paren
+       }
     
     
     
     
-    telephoneCheck("1(555)555-5555");
+    
+    else{
+         return true;
+       }
+       
+    
+    
+    
+    }
+  
+  
+  
+  
+  telephoneCheck("1 555)555-5555");
