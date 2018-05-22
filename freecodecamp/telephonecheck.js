38bdc3d151020af70351c448af07390443ef5525
ilya0 <iosovets@gmail.com>
2018-05-21 23:03:18 -0700

    added answer

--- freecodecamp/telephonecheck.js
+++ freecodecamp/telephonecheck.js
@@ -1,5 +1,13 @@
 
 function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+telephoneCheck("555-555-5555");
+
+=====
+
+function telephoneCheck(str) {
   //take out -
   // taje out ( and  )
   // take out spaces
