ba14e5d164bbe7a275389bf5e7a9ccb10abf55e1
ilya0 <iosovets@gmail.com>
2018-05-22 22:10:45 -0700

    sum primes

--- freecodecamp/sumprimes.js
+++ freecodecamp/sumprimes.js
@@ -1,21 +1,30 @@
 
 function sumPrimes(num) {
-    var holdarray =[];
-   
-    for(var i = 0; i<num; i++){
-      if(i%2!==0 || i%3!==0 || i%5!==0 || i%7!==0 ){
-        console.log("prime number is "+i);
-        holdarray.push(i);
-      }
-      
-      
-    }
-    
-    return holdarray.reduce( 
-      function(total, amount){
-        return total + amount;
-      }
-    );
-  }
-  sumPrimes(10); // 1,3,5,7,9
+  var arraything = [];
   
+  for (var counter = 0; counter <= num; counter++){
+      var notPrime = false;
+  
+      for (var i = 2; i <= counter; i++) {
+        
+          if (counter%i===0 && i!==counter) {
+              notPrime = true;
+              
+          }
+      }
+    
+      if (notPrime === false) {
+          console.log(counter);
+          arraything.push(counter);
+      }
+    
+  }
+  
+      
+      return arraything.reduce(function(total, amount){
+          return total + amount;
+        }
+      )-1;
+    }
+    sumPrimes(977); // 1,3,5,7,9
+    
