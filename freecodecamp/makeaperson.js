a2ecacf00e022a0f1882f45ac5f14d455744f1cd
ilya0 <iosovets@gmail.com>
2018-05-25 16:29:48 -0700

    make a person

--- freecodecamp/makeaperson.js
+++ freecodecamp/makeaperson.js
@@ -1,14 +1,23 @@
 
+
+
 var Person = function(firstAndLast) {
-    // Complete the method below and implement the others similarly
-  var firstName = firstAndLast.split(" ");
-  var lastName;
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.getFirstName = function(firstName){};
-  this.getFullName = function(lastName) { return firstName + lastName;};
   
-    return firstName;
+  var fullNameArray = firstAndLast.split(" ");
+  console.log(fullNameArray);
+  this.setFirstName = function(firstName){ fullNameArray[0] = firstName;};
+  this.setLastName = function(lastName){ fullNameArray[1] = lastName;};
+  this.setFullName = function(fandl){ 
+    fullNameArray = fandl.split(" ");
+  return fullNameArray;};
+  
+  
+  this.getFullName = function(){ return fullNameArray;}; 
+  this.getFirstName = function(){ return  fullNameArray[0];};
+  this.getLastName  = function(){ return fullNameArray[1];};
+  this.getFullName = function() { return fullNameArray[0] +" " +fullNameArray[1] ;
+};
+    return firstAndLast;
 };
 
 var bob = new Person('Bob Ross');
