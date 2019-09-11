0bd61a297c0a8643bf8a11fe449f9a3834a2788f
ilya0 <iosovets@gmail.com>
2019-09-11 15:13:52 -0700

    added median

--- hackerrank/runningmedian.js
+++ hackerrank/runningmedian.js
@@ -1,35 +1,50 @@
 
 
 
+function runningMedian(a){
+//take the array and save it
+// start a counter
+//go through each array index and add it to the 
+var arrayA = a; // this is to store the whole incoming array
+var storagearray = []; // store vars to see if they are odd or even
+console.log("arrayA is", arrayA);
 
-function input(a){
+for(let i=0; i < arrayA.length; i++){ //iterate through the array and create a running median
+   
+    for(let j=0; j<=i; j++){//push all the elements up to current element
+        storagearray.push(arrayA[j]);
+    }
 
-var storagearray = [];
-storagearray.push(a);
-storagearray.sort();
+    storagearray.sort();
+    // console.log("currently storage array is ", storagearray);
+    // console.log("storagearray length is", storagearray.length);
 
-var arraylength = storagearray.length();
+    if(storagearray.length == 0){
+        alert("error");
+    }
+    else if(storagearray.length == 1){
+        console.log(storagearray)
+        console.log("median for one element is", storagearray[0]);
+        storagearray = [];
 
-if(arraylength == 1){
- return storagearray[0];
-}else if ( arraylength % 2 == 0) {
-    alert('Even Number');
-    var split = arraylength /2;
-    var median = (storagearray[split]+storagearray[split+1])/2
-}else{
-    alert('Odd Number');
-    var median = storagearray[Math.round((storagearray.length - 1) / 2)];
-    return median;
-	
+    }else if ( storagearray.length % 2 == 0) {
+        alert('Even Number');
+        console.log(storagearray)
+        var split = storagearray.length /2;
+       
+        var median = (storagearray[split]+storagearray[split+1])/2
+        console.log("median for even is", median); 
+        storagearray = [];
+
+    }else{
+        alert('Odd Number');
+        console.log(storagearray)
+        var median = storagearray[Math.round((storagearray.length - 1) / 2)];
+        storagearray = [];
+        console.log("median odd number elements is", median);
+    }
+
+
 }
 
-
-
-    //add to array
-    // sort array
-    //see if array is odd or even
-        //if odd or 1 
-            //find middle and return
-        //if even 
-            //find center two, and calculate median
 }
