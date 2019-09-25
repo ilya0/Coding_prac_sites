1aaed57c25c2b14ff0975d2aa718f7fbbd1c21ef
ilya0 <iosovets@gmail.com>
2019-09-25 14:29:06 -0700

    added balanced brackets

--- hackerrank/balancedbrackets.js
+++ hackerrank/balancedbrackets.js
@@ -1,14 +1,31 @@
-isBalanced(s){
-    
-    if(s.length%2 == 0){
+function isBalanced(s){
+
+    if(s.length%2 != 0){
         return "not balanced"
     }else{
-        //strip off all chars and place in array
-        // compair to corresponding array pair
-        
+
+       for(let i = 0; i < s.length; i++){
+
+        var inner = s[i] // this grabs the inside bracket
+        var outer = s[(s.length-1)-i] // outside bracket
+        console.log("inner "+inner);
+        console.log("outer "+outer);
+            if( i == s.length-i){ // if outer is same as innner
+                return "YES"
+            }else{
+                if(inner == "[" && outer == "]"){
+                    console.log("matched");
+                }else if(inner == "{" && outer == "}"){
+                    console.log("matched");
+                }else if(inner == "(" && outer == ")"){
+                    console.log("matched")
+                }else{
+                    return "NO"
+                }
+
+            }
+
+       }
     
     }
-    //if amount of brackets is odd, then not balanced
-    //if even
-
 }
