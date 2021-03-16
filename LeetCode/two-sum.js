49667330393ecb49be5435a8609cf4798851d904
ilya0 <iosovets@gmail.com>
2021-03-16 10:09:45 -0700

    working

--- LeetCode/two-sum.js
+++ LeetCode/two-sum.js
@@ -5,6 +5,10 @@
  */
 
 
+ twoSum( [1,2,3,4], 5);
+
+
+
 var twoSum = function(nums, target) {
     //create var to hold first place
     //create var b to sold second place
@@ -13,36 +17,32 @@ var twoSum = function(nums, target) {
     // if that doesn't work then go to 2+n 
     //then the next, untill it sums
 
- 
     var counterarray = [];
 
-
-var testsum = function(){
-
-
-for(start = 0; start < nums.length(); start++){
-
-    for(end = 1; end < nums.length(); end++){
-
-        if(nums[start] + nums[end] == target){
-
-            counterarray.push(start);
-            counterarray.push(end);
-        
-                return (counterarray)
-            }else{
-            
-            }
-    }
-
-
+    for(start = 0; start < nums.length; start++){
     
-    }
+        
+        for(end = 1; end < nums.length; end++){
 
-}
+            if(nums[start] + nums[end] == target){
+
+                counterarray.push(start);
+                counterarray.push(end);
+            console.log(start,end);
+                    return (counterarray)
+                }else{
+                    console.log("cycled")
+                }
+        }
+
+
+        
+        }
 
 
 
 
+
+console.log("this is the end");
 
 };
