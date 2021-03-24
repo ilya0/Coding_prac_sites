366214f6913efe4cc0b9540e502873f5240431c8
ilya0 <iosovets@gmail.com>
2021-03-24 14:08:51 -0700

    added not work

--- LeetCode/prefix.js
+++ LeetCode/prefix.js
@@ -3,26 +3,29 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    
-    var prefix = strs[0].substring(0,2); //set the first prefix to get checked
-    console.log("prefix is "+prefix);
-    var isprefixmatching = false;
+    if(strs.length = 0){return ""};
+       
+       
+       var prefix = strs[0].substring(0,2); //set the first prefix to get checked
+       console.log("prefix is "+prefix);
+       
+       var isprefixmatching = false;
+      
+       
+       for(i=0; i < strs.length; i++ ){
+           if( prefix == strs[i].substring(0,2) ){  
+               isprefixmatching = true; 
+               console.log(prefix)
+               console.log(strs[i].substring(0,2))
    
-    
-    for(i=0; i < strs.length; i++ ){
-        if( prefix == strs[i].substring(0,2) ){  
-            isprefixmatching = true; 
-            console.log(prefix)
-            console.log(strs[i].substring(0,2))
-
-            }else{
-                return ""
-            }
-    }
-    
-    
-    
-    if(isprefixmatching = true){ return prefix; } // check for if all prefixes match
-        
-    
-};
+               }else{
+                   return ""
+               }
+       }
+       
+       
+       
+       if(isprefixmatching = true){ return prefix; } // check for if all prefixes match
+           
+       
+   };
